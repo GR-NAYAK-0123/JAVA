74741e833adddf6c7aa3fb06fa39e9849d0ab7ba
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-07-18 02:56:43 +0000

    Value pass to the parameter

---
+++ Demo/03_Basic.java
@@ -0,0 +1,31 @@
+/*
+       Q) How to pass value for the parameter which is present in a method ?
+       Ans) We need pass the value for the parameter whenever we are calling that method
+ */
+
+package Demo;
+
+class ClassA {
+    void meth1(int a, int b){
+        System.out.println("meth1() called");
+        System.out.println(a);
+    }
+    int meth2(){
+        System.out.println("meth2() called");
+        return 100;
+    }
+    int meth3(int a, int b, int c){
+        System.out.println("meth3() called");
+        return a+b+c;
+    }
+    public static void main(String[] args) {
+        System.err.println("Start");
+        ClassA aobj = new ClassA();
+        aobj.meth1(10, 20);
+        int result = aobj.meth2();
+        System.out.println("result : "+result);
+        System.out.println("------------------------------");
+        System.out.println("Value is : "+aobj.meth3(5,7,8));
+        System.out.println("End");
+    }
+}
