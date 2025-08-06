5eef20dd56b971d70145d391f01ef9c4c475e83e
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-08-06 20:03:47 +0000

    Program for two dimenssional array

---
+++ ARRAY/TwoDimenssionArray.java
@@ -0,0 +1,18 @@
+package ARRAY;
+
+public class TwoDimenssionArray {
+    void meth1(){
+        int arr[][] = {{1, 2, 3}, {4, 5, 6},{7, 8, 9}};
+
+        //Retrieving the data from two dimenssional array by using for loop
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        new TwoDimenssionArray().meth1();
+    }
+}
