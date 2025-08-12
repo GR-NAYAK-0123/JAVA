85ae442c9361f591a272fb7446df1c4cd6f8111d
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-08-12 09:54:10 +0000

    Added some notes

--- MULTITHREADING/Third_Thread.java
+++ MULTITHREADING/Third_Thread.java
@@ -26,3 +26,19 @@ public class Third_Thread extends Thread{
         t2.start();
     }
 }
+
+/*
+   Thread Scheduler :
+  --------------------
+   => It will decide which thread should starts its execution basing upon two things. 
+         1) Thread priorities. 
+         2) Underlying OS (Hardware apparatus)
+
+    
+   Thread priorities :
+  ---------------------
+   => It is varies between 1 to 10
+        Minimum priority - 1
+        Average priority - 5
+        Maximum priority - 10 
+ */
