40b3617f17e71c86bc9bde82562f231bfa1b35a6
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-08-12 09:19:32 +0000

    This is about a Thread

---
+++ MULTITHREADING/Thread1.java
@@ -0,0 +1,42 @@
+/*
+    What is a Thread :
+   --------------------
+   => We can create a thread inside a process. 
+   => Process act as the host for a thread. 
+   => Atleast one process is required to create a thread. 
+   => Thread shares same memory address. 
+   => Context switching is easy in threads. 
+
+   How to create a Thread ?
+  --------------------------
+   => We can create a thread in any one of the below mentioned ways
+       1) By implementing Runnable interface
+       2) By extending Thread class
+
+    Note :
+   -------
+    => Whenever we are starting a thread with the help of start(), every thread by default it will execute run(). 
+
+    Steps for creating a thread :
+   -------------------------------
+    => Write a class that extends Thread class or implements Runnable interface this is available in java.lang package. 
+    => Write public void run() method in that class, this is the method by default executed by any method. 
+    => Create an object to that class. (Inside the main method)
+    => Create a thread and attach it to the object. 
+    => Start running the thread. 
+    
+ */
+
+package MULTITHREADING;
+
+public class Thread1 extends java.lang.Thread {
+    public void run(){
+        System.out.println("Run method called");
+    }
+    public static void main(String[] args) {
+        Thread1 obj = new Thread1();
+        java.lang.Thread t1=new java.lang.Thread(obj);
+        t1.start();
+        //obj.start();
+    }
+}
