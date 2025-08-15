2ae0a2d45779b47ebef2203c1237763231b7ff5a
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-08-15 12:34:34 +0000

    All about wait and notify method

---
+++ MULTITHREADING/Wait_Notify.java
@@ -0,0 +1,48 @@
+package MULTITHREADING;
+
+public class Wait_Notify {
+    int amount = 10000;
+
+    synchronized void with_draw(int amount) throws InterruptedException {
+        if(this.amount < amount){
+            System.out.println("Insufficient balance");
+            wait();
+            System.out.println("Amount credited");
+            this.amount -= amount;
+            System.out.println("Withdraw successfully \n Balance is : "+this.amount);
+
+        } else {
+            this.amount -= amount;
+            System.out.println("Withdraw successfully \n Balance is : "+this.amount);
+        }
+    }
+    synchronized void deposit(int amount){
+        this.amount += amount;
+        System.out.println("Deposit successfully \n Balance is : "+this.amount);
+        notify();
+    }
+}
+
+
+class Withdraw {
+    public static void main(String[] args) {
+        Wait_Notify obj = new Wait_Notify();
+
+        new Thread() {    // Anonymous Inner class starts here
+            public void run(){
+                try{
+                    obj.with_draw(15000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }.start();   // Anonymous Inner class ends here
+
+        new Thread() {     // Anonymous Inner class starts here
+            public void run(){
+                obj.deposit(10000);
+            }
+        }.start();   // Anonymous Inner class ends here
+    } 
+}
