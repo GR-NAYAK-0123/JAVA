ec32f170614e00d1a50b38ce72bf63332f2cf796
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2025-09-07 02:32:35 +0000

    Just modified a comment

--- COLLECTION_FRAMEWORK/TreeMap_1.java
+++ COLLECTION_FRAMEWORK/TreeMap_1.java
@@ -9,7 +9,7 @@ public class TreeMap_1 {
         tm.put(10,10);   //It doesn't maintain the insertion order
         tm.put(20,"java");  // Heterogeneous key are not allowed but values are allowed
         //tm.put("java,20");         // Null keys are not allowed but values are allowed
-        //tm.put(null,null);        // Duplicates are not allowed
+        //tm.put(null,null);        // Duplicates are allowed
         tm.put(30,null);  // It is available from Java 1.2v
         tm.put(40,"css");  // Default capacity is 16, Loadfactor is 0.75 and size increases by double
         tm.put(50,"europe");  //It is not synchronized
